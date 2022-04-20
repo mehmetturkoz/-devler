e7088102278aa23d0182da0a2676d1591aedfb5f
mehmet turkoz <mturkozz42@gmail.com>
2022-04-20 12:22:36 +0300

    Sayı sıralama ödevi yapıldı.

---
+++ java101odevler/SayiSiralama.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+public class SayiSiralama {
+    public static void main(String[] args){
+
+        int x,y,z;
+        Scanner input=new Scanner(System.in);
+        x =input.nextInt();
+        y =input.nextInt();
+        z = input.nextInt();
+
+        if(x>y && x>z){
+            if(y>z){
+                System.out.println("x>y>z");
+            }else
+                System.out.println("x>z>y");
+        }else if(y>x && y>z){
+            if(x>z){
+                System.out.println("y>x>z");
+            }else{
+                System.out.println("y>z>x");
+            }
+        }else if(z>x && z>y){
+            if(x>y){
+                System.out.println("z>x>y");
+            }else{
+                System.out.println("z>y>x");
+            }
+        }
+
+
+    }
+}
